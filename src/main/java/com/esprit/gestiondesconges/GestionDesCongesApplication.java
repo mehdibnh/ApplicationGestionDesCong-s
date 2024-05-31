a59ae3764d422820a546c296f87d017caa3456aa
Mehdi <mehdi@example.com>
2024-05-31 12:11:15 +0100

    Initial commit

---
+++ src/main/java/com/esprit/gestiondesconges/GestionDesCongesApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.gestiondesconges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesCongesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDesCongesApplication.class, args);
+    }
+
+}
